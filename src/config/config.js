d4319984ee62675f1e02c6c9f3e3612d30983896
RavenaStar <ravenastar@proton.me>
2024-09-09 12:12:58 -0300

    🦋🟢

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ const TG = process.env.TAG;
 
 const MAX_REQUESTS_PER_HOUR = 1666; // Limite de 1.666 registros por hora
 const MAX_REQUESTS_PER_EXECUTION = 300; // Limite de 300 solicitações por execução do CronJob
-const cronMinutes = 8;
+const cronMinutes = 15;
 
 const MAX_REQUESTS_DAILY = 300;
 const MAX_POINTS_PER_HOUR = 5000; // Limite de pontos por hora
