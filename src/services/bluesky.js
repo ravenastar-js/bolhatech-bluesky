6ec93235ebd7fb54399a5be721174dd0b5e5a325
RavenaStar <ravenastar@proton.me>
2024-09-09 15:44:24 -0300

    🦋🟢

--- src/services/bluesky.js
+++ src/services/bluesky.js
@@ -1,6 +1,5 @@
 require('../config/dotenv.js');
 const axios = require('axios');
-const { delay } = require('../utils/delay.js')
 const { API_URL, TG, MAX_REQUESTS_PER_HOUR, MAX_REQUESTS_PER_EXECUTION, cronMinutes, MAX_POINTS_PER_HOUR, embed_color, embed_bannerURL, wh_avatarURL, wh_username } = require('../config/config');
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, WebhookClient, EmbedBuilder } = require('discord.js');
 
@@ -264,6 +263,7 @@ async function main() {
 
         // 🔄 Repost all unreposted posts
         for (const post of unrepostedPosts) {
+            const { delay } = require('../utils/delay')
             await repost(post, token, did);
             await delay(delayTime); // ⏰ delay time
         }
