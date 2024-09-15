a31b0e198a498903e15b2920aa1e5326730e4a2a
RavenaStar <ravenastar@proton.me>
2024-09-15 17:45:00 -0300

    🦋🟢

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,13 @@ require('./dotenv');
 const API_URL = 'https://bsky.social/xrpc';
 const TG = process.env.TAG;
 
+function convertString(tags) {
+    let words = tags.split(',');
+    return words.length === 1 ? tags : words.map(word => `${word}`).join('%20');
+}
+
+const LUCENE = convertString(TG)
+
 const MAX_REQUESTS_PER_HOUR = 1666; // Limite de 1.666 registros por hora
 const MAX_REQUESTS_PER_EXECUTION = 300; // Limite de 300 solicitações por execução do CronJob
 const cronMinutes = 8;
@@ -17,6 +24,7 @@ const wh_username = process.env.BLUESKY_USERNAME
 module.exports = {
     API_URL,
     TG,
+    LUCENE,
     MAX_REQUESTS_PER_HOUR,
     MAX_REQUESTS_PER_EXECUTION,
     cronMinutes,
